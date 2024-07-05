878e2303fd181fd4bd785d07cb78499b8509eb41
Diptimayee777 <diptimayeepatra07@gmail.com>
2024-07-05 15:55:46 +0530

    CurrencyConverter.py

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -15,20 +15,20 @@ def clicked():
     label4 = tk.Label(window, text=str(data), font="Times 16 bold")
     label4.place(x=200, y=300)
 
-label = tk.Label(window, text="Currency Converter", font="Times 20 bold")
-label.place(x=120, y=40)
+label = tk.Label(window, text="CURRENCY CONVERTER", font="Times 22 bold")
+label.place(x=80, y=40)
 
-label1 = tk.Label(window, text="Enter amount here:", font="Times 10 bold")
+label1 = tk.Label(window, text="Enter amount here:", font="Times 15 bold")
 label1.place(x=70, y=100)
 entry1 = tk.Entry(window)
 entry1.place(x=270, y=105)
 
-label2 = tk.Label(window, text="Enter your currency here:", font="Times 10 bold")
+label2 = tk.Label(window, text="Enter your currency here:", font="Times 15 bold")
 label2.place(x=30, y=150)
 entry2 = tk.Entry(window)
 entry2.place(x=270, y=155)
 
-label3 = tk.Label(window, text="Enter your desired currency:", font="Times 10 bold")
+label3 = tk.Label(window, text="Enter your desired currency:", font="Times 15 bold")
 label3.place(x=15, y=200)
 entry3 = tk.Entry(window)
 entry3.place(x=270, y=205)
